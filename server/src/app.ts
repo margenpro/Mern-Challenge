ad00be216c865aaeae60ee29d03bf1c31a94fd0f
mgentiletag <mgentile@taglatam.com>
2021-10-14 14:28:32 -0300

    added build and heroku

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,13 +6,18 @@ import mongoose from "mongoose";
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Welcome to server");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../../client/build"));
+}
+
 app.listen(port, () => {
   return console.log(`Server running on Port: ${port}`);
 });
